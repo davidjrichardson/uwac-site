bcc0e7300b33b501722af6c21864a9767b5bc8d7
David Richardson <david@tankski.co.uk>
2017-09-11 19:15:04 +0100

    Add custom styling for top nav and finish jumbotron

--- archery/settings/dev.py
+++ archery/settings/dev.py
@@ -11,6 +11,7 @@ SECRET_KEY = '3+&3a^7nypjh%t(_fjhr95l5#-k)!9_o7@1b#0pphp$=a(@_3+'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+ALLOWED_HOSTS = ['*']
 
 try:
     from .local import *
