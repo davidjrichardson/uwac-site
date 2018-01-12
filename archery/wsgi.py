7bb8962aabb16bd30b0c011d83ec4ab91dd96418
David Richardson <david@tankski.co.uk>
2018-01-12 15:44:39 +0000

    Update WSGI config

--- archery/wsgi.py
+++ archery/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "archery.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "archery.settings.production")
 
 application = get_wsgi_application()
