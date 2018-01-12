74faa38e7ecabc3ace23b6c022eaf9396f6dbcb6
David Richardson <david@tankski.co.uk>
2018-01-12 21:24:22 +0000

    Update stylesheets

--- archery/settings/dev.py
+++ archery/settings/dev.py
@@ -3,9 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-COMPRESS_ENABLED = True
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '3+&3a^7nypjh%t(_fjhr95l5#-k)!9_o7@1b#0pphp$=a(@_3+'
