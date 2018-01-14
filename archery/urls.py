dce3f70ab32e849b8c8fd343046722a071578da2
David Richardson <david@tankski.co.uk>
2018-01-14 10:27:52 +0000

    Add sitemap.xml generation

--- archery/urls.py
+++ archery/urls.py
@@ -4,19 +4,18 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from wagtail.contrib.wagtailsitemaps.views import sitemap
+
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
-from search import views as search_views
-
 urlpatterns = [
+    url(r'^sitemap\.xml$', sitemap),
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^cms/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
-
-    url(r'^search/$', search_views.search, name='search'),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
@@ -27,7 +26,6 @@ urlpatterns = [
     # of your site, rather than the site root:
     #    url(r'^pages/', include(wagtail_urls)),
 ]
-
 
 if settings.DEBUG:
     from django.conf.urls.static import static
