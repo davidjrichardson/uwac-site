f8f570ad6a81c347b2383881146f07991525b506
David <david@tankski.co.uk>
2023-08-27 21:30:28 +0100

    chore: Create dockerfile and docker compose manifest

--- archery/settings/dev.py
+++ archery/settings/dev.py
@@ -5,14 +5,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3+&3a^7nypjh%t(_fjhr95l5#-k)!9_o7@1b#0pphp$=a(@_3+'
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-ALLOWED_HOSTS = ['*']
-
 try:
     from .local import *
 except ImportError:
